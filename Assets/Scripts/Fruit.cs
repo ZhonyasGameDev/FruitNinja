4e47acade2c191dfc57b282635ea5ce1a1612a4d
ZhonyasGameDev <zhonyasgamedev@gmail.com>
2024-08-05 14:50:30 -0600

    Added Particle Effect
    When a fruit is sliced, the particle system is activated.

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -8,11 +8,13 @@ public class Fruit : MonoBehaviour
 
     private Rigidbody fruitRigidBody;
     private Collider fruitCollider;
+    private ParticleSystem juiceParticleEffect;
 
     private void Awake()
     {
         fruitRigidBody = GetComponent<Rigidbody>();
         fruitCollider = GetComponent<Collider>();
+        juiceParticleEffect = GetComponentInChildren<ParticleSystem>();
     }
 
     private void Slice(Vector3 direction, Vector3 position, float force)
@@ -21,6 +23,7 @@ public class Fruit : MonoBehaviour
         fruitSliced.SetActive(true);
 
         fruitCollider.enabled = false;
+        juiceParticleEffect.Play();
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         fruitSliced.transform.rotation = Quaternion.Euler(0f, 0f, angle);
